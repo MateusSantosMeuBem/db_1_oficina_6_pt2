b658ccf07601d02ca1919ac6e2645a2d18136fa2
MateusSantosMeuBem <a.mateussm1.5@gmail.com>
2022-07-26 14:57:43 -0300

    :wrench: | Select user to connect

--- app/__main__.py
+++ app/__main__.py
@@ -25,22 +25,36 @@ manager = [
 ]
 
 try:
-    print('Trying conecting...')
-    connection = connector()
-    print('Conected!')
+    from getpass import getpass
 
-    while True:
-        choice = draw_menu()
-        if choice in [str(option) for option in range(1, len(manager) + 2)]:
-            if choice == str(len(options)):
-                break
+    db_user: str = input('Digite o nome do usuário: ')
+    pass_01: str = getpass('Digite a senha do usuário: ')
+    pass_02: str = getpass('Digite a senha novamente: ')
+
+    if pass_01 == pass_02:
+
+        print('Trying conecting...')
+        connection = connector(
+            db_user = db_user,
+            senha_usuario_sgbd = pass_01
+        )
+        del db_user, pass_01, pass_02
+        print('Conected!')
+
+        while True:
+            choice = draw_menu()
+            if choice in [str(option) for option in range(1, len(manager) + 2)]:
+                if choice == str(len(options)):
+                    break
+                else:
+                    manager[int(choice) - 1](connection)
             else:
-                manager[int(choice) - 1](connection)
-        else:
-            print('\n -- DIGITE UMA OPÇÃO VÁLIDA -- \n')
+                print('\n -- DIGITE UMA OPÇÃO VÁLIDA -- \n')
 
-    connection.close()
-    print('Connection closed!')
+        connection.close()
+        print('Connection closed!')
+    else:
+        raise Exception('A senhas não são iguais!')
 
 except Exception as exception:
     print(exception)
--- app/main/model/__init__.py
+++ app/main/model/__init__.py
@@ -1,17 +1,17 @@
 from main import ROOT_PATH
+import os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=f'{ROOT_PATH}/.env')
 
-def connector():
-    from mysql.connector import connect
-    import os
-    from dotenv import load_dotenv
-
-    load_dotenv(dotenv_path=f'{ROOT_PATH}/.env')
-
-    endereco_ip_sgbd: str = os.getenv('dbms_ip_address')
-    nome_esquema: str = os.getenv('schema_name')
-    db_user: str = os.getenv('user_dbms')
-    senha_usuario_sgbd: str = os.getenv('password_dbms')
+def connector(
+    endereco_ip_sgbd: str = os.getenv('dbms_ip_address'),
+    nome_esquema: str = os.getenv('schema_name'),
+    db_user: str = os.getenv('user_dbms'),
+    senha_usuario_sgbd: str = os.getenv('password_dbms'),
     dp_port: str = os.getenv('port')
+):
+    from mysql.connector import connect
+
 
     return connect(
         host=endereco_ip_sgbd, 
